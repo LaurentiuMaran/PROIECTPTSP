a586895f1820bca3bfff7099bbb279454c1b1091
Laurentiu Maran <laurentiu.maran01@e-uvt.ro>
2022-01-06 20:01:56 +0200

    commit lab 4

--- ProjectSP/src/Element.java
+++
@@ -1,7 +0,0 @@
-public abstract class Element {
-
-    public abstract void print();
-    public abstract void add(Element e);
-    public abstract void remove(Element e);
-    public  abstract  Element get(Integer e);
-}
